9144526aec5c477e99f97cdf4cd1d439be1bdb7b
sandy1999 <sandyrockstar19@gmail.com>
2018-08-07 01:20:34 +0530

    added: Support vector regressor

---
+++ Regression/svr.py
@@ -0,0 +1,28 @@
+
+# base import
+import numpy as np # matrix maths 
+import pandas as pd # for data handeling 
+import matplotlib.pyplot as plt # for plotting data 
+
+# sklearn imports 
+from sklearn.svm import SVR # svm regression
+
+# import data set 
+data = pd.read_csv('./Position_Salaries.csv')
+features = data[['Level']] # features (X) 
+output = data[['Salary']] # output i.e. y
+
+# model fitting for regressor
+regressor = SVR(kernel='poly')
+regressor.fit(features,output)
+
+# making a prediction score 
+regressor.score(features,output)
+
+# making a data viz 
+plt.scatter(features,output, color='red')
+plt.plot(features, regressor.predict(features))
+plt.title('Position vs Salary')
+plt.xlabel('Position')
+plt.ylabel('Salary')
+plt.show()
